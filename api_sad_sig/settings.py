09cd06edaf37b4ab8e0cd41428e8394530cb8a94
invarii <invaridewi@gmail.com>
2020-10-24 16:42:12 +0700

    Update setting and deploy script

--- api_sad_sig/settings.py
+++ api_sad_sig/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = env.str('STATIC_ROOT')
+
 STATIC_URL = '/static/'
 
 # CORS_ORIGIN_WHITELIST = [
