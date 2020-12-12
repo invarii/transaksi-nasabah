cb4f52ba3494e6c1d70ef17b74ca2653a6816f7e
Agung Wiyono <wiyonoagung1@gmail.com>
2020-12-12 09:40:55 +0700

    Update user creation and update

--- users/serializers.py
+++ users/serializers.py
@@ -11,23 +11,43 @@ class GroupSerializer(DynamicModelSerializer):
 
 class UserSerializer(DynamicModelSerializer):
     role = fields.CharField(source="groups.first")
+    group_name = fields.CharField(write_only=True)
 
     class Meta:
         model = User
         name = "data"
-        fields = ["id", "username", "email", "groups", "role"]
+        fields = ["id", "username", "email", "role", 'group_name', 'password']
+        read_only_fields = ['role']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        try:
+            group = Group.objects.get(name=validated_data['group_name'])
+        except Exception:
+            group = Group.objects.get(name='penduduk')
+        validated_data.pop('group_name')
+
         user = super().create(validated_data)
         user.is_active = True
         user.set_password(validated_data["password"])
+
+        user.groups.add(group)
         user.save()
 
         return user
 
     def update(self, instance, validated_data):
+        try:
+            group = Group.objects.get(name=validated_data['group_name'])
+        except Exception:
+            group = Group.objects.get(name='penduduk')
+        validated_data.pop('group_name')
+
         user = super().update(instance, validated_data)
         user.set_password(validated_data["password"])
+
+        user.groups.remove(user.groups.first())
+        user.groups.add(group)
         user.save()
 
         return user
