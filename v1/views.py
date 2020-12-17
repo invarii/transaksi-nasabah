1a6dffba059f3d752607c2236066b9af440d1f22
invarii <64796357+invarii@users.noreply.github.com>
2020-12-17 11:36:54 +0700

    Development (#52)
    * Batas Desa
    
    * db batasdesa

--- v1/views.py
+++ v1/views.py
@@ -20,6 +20,7 @@ from .serializers import (
     SadKabKotaSerializer,
     SadKecamatanSerializer,
     SadDesaSerializer,
+    BatasDesaSerializer,
     SadDusunSerializer,
     SadRwSerializer,
     SadRtSerializer,
@@ -72,6 +73,7 @@ from .models import (
     SadKabKota,
     SadKecamatan,
     SadDesa,
+    BatasDesa,
     SadDusun,
     SadRw,
     SadRt,
@@ -176,6 +178,10 @@ class PegawaiViewSet(CustomView):
     serializer_class = PegawaiSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+class BatasDesaViewSet(CustomView):
+    queryset = BatasDesa.objects.all().order_by("id")
+    serializer_class = BatasDesaSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class SadKabKotaViewSet(CustomView):
     queryset = SadKabKota.objects.all().order_by("id")
