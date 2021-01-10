49c3bf34446710b09c74ff4eb6afc16ef1172acb
invarii <invaridewi@gmail.com>
2021-01-10 14:51:03 +0700

    gmbar bidang

--- v1/views.py
+++ v1/views.py
@@ -481,6 +481,8 @@ class SigBidangViewSet(CustomView):
                 {
                     "bidang": i.bidang.id,
                     "nbt": i.bidang.nbt,
+                    "gambar_depan": i.bidang.gambar_depan,
+                    "gambar_atas": i.bidang.gambar_atas,
                     "namabidang": i.namabidang,
                 }
                 for i in kepemilikan
