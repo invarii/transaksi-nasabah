16c49b20e4cb02f93fb05b0e64fc2d9f5d457c03
invarii <invaridewi@gmail.com>
2021-01-29 14:33:44 +0700

    fix params penduduk di pecah kk

--- layananperistiwa/serializers.py
+++ layananperistiwa/serializers.py
@@ -458,8 +458,9 @@ class MiniPendudukSerializer(serializers.Serializer):
 
 class SadPecahKKSerializer(CustomSerializer):
     no_kk = serializers.CharField(write_only=True)
-    anggota_kk = DynamicRelationField(
-        "SadPendudukSerializer", deferred=True, embed=True
+    anggota_kk = MiniPendudukSerializer(many=True, write_only=True)
+    penduduk = DynamicRelationField(
+        "MiniUserSerializer", deferred=True, embed=True
     )
     rt = serializers.IntegerField(write_only=True)
 
